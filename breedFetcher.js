01f920b02342a09fbe07e8f3002f8931b116bfc3
amusictheorist <a_millaresthomson@outlook.com>
2024-09-26 19:08:33 -0400

    Refactoring code

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,18 @@ const baseUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
 const breed = process.argv[2];
 const completeUrl = baseUrl + breed;
 
-needle.get(completeUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Failed to fetch breed details: ${error}`);
-    return;
-  }
-  if (body.length > 0) {
-    console.log(`Breed Description: ${body[0].description}`);
-  } else {
-  console.log("Breed not found.");
-  }
-});
+const fetchBreedDescription = function (breed, callback) {
+  needle.get(completeUrl, (error, response, body) => {
+    if (error) {
+      console.error(`Failed to fetch breed details: ${error}`);
+      return;
+    }
+    if (body.length > 0) {
+      console.log(`Breed Description: ${body[0].description}`);
+    } else {
+      console.log("Breed not found.");
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
