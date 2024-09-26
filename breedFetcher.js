089b169756e6736210422ff2acaec270b36802d7
amusictheorist <a_millaresthomson@outlook.com>
2024-09-26 17:53:03 -0400

    First commit

---
+++ breedFetcher.js
@@ -0,0 +1,16 @@
+const needle = require("needle");
+const baseUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
+const breed = process.argv[2];
+const completeUrl = baseUrl + breed;
+
+needle.get(completeUrl, (error, response, body) => {
+  if (error) {
+    console.error(`Failed to fetch breed details: ${error}`);
+    return;
+  }
+  if (body.length > 0) {
+    console.log(`Breed Description: ${body[0].description}`);
+  } else {
+  console.log("Breed not found.");
+  }
+});
